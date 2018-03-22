b06a95d4dc4ab3b03f175400dc5d71b421ae02e9
cmartineztoral <carlos.martineztoral@telefonica.com>
2018-03-22 18:28:33 +0100

    First version of the Android app to talk to DialogFlow

---
+++ voiceApiConversion/src/main/java/equinox/sweethomebulbs/voiceApiConversion/VoiceToApi.java
@@ -0,0 +1,158 @@
+package equinox.sweethomebulbs.voiceApiConversion;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIOutputContext;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+import com.google.gson.JsonElement;
+
+import java.util.List;
+import java.util.Map;
+
+public class VoiceToApi extends AppCompatActivity implements AIListener {
+
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextView mVoiceInputTv;
+    private ImageButton mSpeakBtn;
+
+    private AIService aiService;
+
+    final AIConfiguration config = new AIConfiguration("1d61e2d21a264a68ad9a29494c7b39c0",
+            AIConfiguration.SupportedLanguages.Spanish,
+            AIConfiguration.RecognitionEngine.System);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_voice_to_api);
+
+        aiService = AIService.getService(this, config);
+        aiService.setListener((AIListener) this);
+
+        mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
+        mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
+        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View view) {
+                requestAudioPermissions();
+            }
+        });
+    }
+
+    @Override
+    public void onResult(final AIResponse response) {
+        Result result = response.getResult();
+
+        // Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        }
+
+        // Show results in TextView.
+        mVoiceInputTv.setText("Query:" + result.getResolvedQuery() +
+                "\nAction: " + result.getAction() +
+                "\nParameters: " + parameterString +
+                "\nIntent: " + result.getResolvedQuery());
+
+        List<AIOutputContext> contexts = result.getContexts();
+    }
+
+    @Override
+    public void onError(final AIError error) {
+        mVoiceInputTv.setText(error.toString());
+    }
+
+    @Override
+    public void onListeningStarted() {
+    }
+
+    @Override
+    public void onListeningCanceled() {
+    }
+
+    @Override
+    public void onListeningFinished() {
+    }
+
+    @Override
+    public void onAudioLevel(final float level) {
+    }
+
+    //Requesting run-time permissions
+
+    //Create placeholder for user's consent to record_audio permission.
+    //This will be used in handling callback
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+
+    private void requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //When permission is not granted by user, show them message why this permission is needed.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
+
+                //Give user option to still opt-in the permissions
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+
+            } else {
+                // Show user dialog to grant permission to record audio
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+            }
+        }
+        //If permission is granted, then go ahead recording audio
+        else if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            //Go ahead with recording audio now
+            aiService.startListening();
+        }
+    }
+
+    //Handling callback
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                    aiService.startListening();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+}
